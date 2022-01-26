b82f034a54deffe29d2fe11caa2ec22579f25eaa
Starinin Andrey <starininandrey@gmail.com>
2022-01-26 17:42:48 +0300

    Init PD011.TestApp

---
+++ PD011.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PD011.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
