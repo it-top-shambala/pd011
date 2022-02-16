15ff6c75b05664f7c46efbbf19bf41509bcc8cc0
Pavel Leonenko <leonenkopavel1985@gmail.com>
2022-02-16 21:11:30 +0700

    Доработка Create tables

--- SQL/CREATE TABLES.sql
+++ SQL/CREATE TABLES.sql
@@ -32,9 +32,9 @@ CREATE TABLE  table_book
     book_title            VARCHAR(100) NOT NULL,
     book_year_of_creation INT          NOT NULL,
     CONSTRAINT FK_book_genre
-    FOREIGN KEY (id_genre) REFERENCES table_genres (id),
+    FOREIGN KEY (id_genre) REFERENCES table_genres (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_book_series
-    FOREIGN KEY (id_series) REFERENCES table_series (id),
+    FOREIGN KEY (id_series) REFERENCES table_series (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     capter_in_series      VARCHAR(100)
 );
 
@@ -43,9 +43,9 @@ CREATE TABLE table_author_book
     id_author       INT NOT NULL,
     id_book         INT NOT NULL,
     CONSTRAINT FK_book_author
-    FOREIGN KEY (id_author) REFERENCES table_authors (id),
+    FOREIGN KEY (id_author) REFERENCES table_authors (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_book
-    FOREIGN KEY (id_book) REFERENCES table_book (id),
+    FOREIGN KEY (id_book) REFERENCES table_book (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     sequences_order VARCHAR(50)
 );
 
@@ -55,9 +55,9 @@ CREATE TABLE table_publication_of_book
     id_publisher           INT    NOT NULL,
     id_book                INT    NOT NULL,
     CONSTRAINT FK_book_publishers
-    FOREIGN KEY (id_publisher) REFERENCES table_publishers (id),
+    FOREIGN KEY (id_publisher) REFERENCES table_publishers (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_book
-    FOREIGN KEY (id_book) REFERENCES table_book (id),
+    FOREIGN KEY (id_book) REFERENCES table_book (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     publication_page_count INT    NOT NULL,
     publication_year       INT    NOT NULL,
     price                  DOUBLE NOT NULL
@@ -75,9 +75,9 @@ CREATE TABLE table_sale_publication_of_book
     id_publication_of_book INT NOT NULL,
     id_sale                INT NOT NULL,
     CONSTRAINT FK_publication_of_book
-    FOREIGN KEY (id_publication_of_book) REFERENCES table_publication_of_book (id),
+    FOREIGN KEY (id_publication_of_book) REFERENCES table_publication_of_book (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_book_sales
-    FOREIGN KEY (id_sale) REFERENCES table_sales (id)
+    FOREIGN KEY (id_sale) REFERENCES table_sales (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE table_users
@@ -98,9 +98,9 @@ CREATE TABLE table_user_role
     id_user INT NOT NULL,
     id_role INT NOT NULL,
     CONSTRAINT FK_user
-    FOREIGN KEY (id_user) REFERENCES table_users (id),
+    FOREIGN KEY (id_user) REFERENCES table_users (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_role
-    FOREIGN KEY (id_role) REFERENCES table_roles (id)
+    FOREIGN KEY (id_role) REFERENCES table_roles (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE table_status_sail
@@ -122,22 +122,22 @@ CREATE TABLE table_seling
     id_user       INT          NOT NULL,
     id_sel_status INT          NOT NULL,
     CONSTRAINT FK_user
-    FOREIGN KEY (id_user) REFERENCES table_users (id),
+    FOREIGN KEY (id_user) REFERENCES table_users (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_book_sel_status
-    FOREIGN KEY (id_sel_status) REFERENCES table_status_sail (id)
+    FOREIGN KEY (id_sel_status) REFERENCES table_status_sail (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE table_buing
 (
     id              INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    sel_date        VARCHAR(100) NOT NULL,
+    buy_date        VARCHAR(100) NOT NULL,
     company_name    VARCHAR(100) NOT NULL,
     id_user         INT          NOT NULL,
     id_buy_status   INT          NOT NULL,
     CONSTRAINT FK_user
-    FOREIGN KEY (id_user) REFERENCES table_users (id),
+    FOREIGN KEY (id_user) REFERENCES table_users (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_book_buy_status
-    FOREIGN KEY (id_buy_status) REFERENCES table_status_buy (id)
+    FOREIGN KEY (id_buy_status) REFERENCES table_status_buy (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE table_sel_publication_of_book
@@ -146,9 +146,9 @@ CREATE TABLE table_sel_publication_of_book
     id_publication_of_book INT          NOT NULL,
     quntity                VARCHAR(100) NOT NULL,
     CONSTRAINT FK_book_sel
-    FOREIGN KEY (id_sel) REFERENCES table_sales (id),
+    FOREIGN KEY (id_sel) REFERENCES table_sales (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_publication_of_book
-    FOREIGN KEY (id_publication_of_book) REFERENCES table_publication_of_book (id)
+    FOREIGN KEY (id_publication_of_book) REFERENCES table_publication_of_book (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE table_buy_publication_of_book
@@ -157,7 +157,7 @@ CREATE TABLE table_buy_publication_of_book
     id_publication_of_book INT          NOT NULL,
     quntity                VARCHAR(100) NOT NULL,
     CONSTRAINT FK_book_buy
-    FOREIGN KEY (id_buy) REFERENCES table_buing (id),
+    FOREIGN KEY (id_buy) REFERENCES table_buing (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT FK_publication_of_book
-    FOREIGN KEY (id_publication_of_book) REFERENCES table_publication_of_book (id)
+    FOREIGN KEY (id_publication_of_book) REFERENCES table_publication_of_book (id) ON UPDATE NO ACTION ON DELETE NO ACTION
 );
